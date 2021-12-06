4e1fe2e8ff40ef372e165331ebc4e69d4f0d6b47
Hao Chen <85007134+xihuan313@users.noreply.github.com>
2021-12-06 00:51:17 -0500

    Update SchoolMax.java

--- src/main/java/SchoolMax.java
+++ src/main/java/SchoolMax.java
@@ -1,11 +1,21 @@
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.html.*;
-
-import javax.lang.model.type.ArrayType;
-import java.io.*;
+import com.gargoylesoftware.htmlunit.javascript.host.xml.XMLSerializer;
+import net.sourceforge.htmlunit.corejs.javascript.EcmaError;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import gui.ava.html.image.generator.HtmlImageGenerator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+
 
 public class SchoolMax{
 
@@ -30,7 +40,7 @@ public class SchoolMax{
         passwordInput.setValueAttribute(password);
 
         HtmlSubmitInput button = (HtmlSubmitInput) page.getFirstByXPath("//*[@id=\"section_4\"]/tr[2]/td[2]/input");
-        page = button.click();
+        page = button.click();}
         return page.getTitleText();
     }
     //return all the classes
@@ -88,6 +98,8 @@ public class SchoolMax{
             HtmlAnchor gradePage = (HtmlAnchor) page.getAnchorByText("Review grades for this class");
             page = gradePage.click();
 
+
+
             String gradePage2 = page.getBody().getVisibleText();
             String grade = gradePage2.substring(gradePage2.indexOf("Current"), gradePage2.indexOf("Period and Term Grade"));
             return grade;
@@ -95,6 +107,34 @@ public class SchoolMax{
             e.printStackTrace();
             String grade = "Error";
             return grade;
+        }
+    }
+    public File grade1(String courseNumber) throws IOException {
+        try {
+            HtmlAnchor gradeBook = (HtmlAnchor) page.getAnchorByText("Gradebook");
+            page = gradeBook.click();
+            String wholePage = page.getVisibleText();
+
+            if (!wholePage.contains(courseNumber)) {
+                return null;
+            }
+
+            HtmlAnchor coursePage = (HtmlAnchor) page.getAnchorByText(courseNumber);
+            page = coursePage.click();
+            HtmlAnchor gradePage = (HtmlAnchor) page.getAnchorByText("Review grades for this class");
+            page = gradePage.click();
+
+            WebResponse response = page.getWebResponse();
+            String html = page.getWebResponse().getContentAsString().substring(page.getWebResponse().getContentAsString().indexOf("<td colspan=\"7\" class=\"subheading2\">Assessment</td>"), page.getWebResponse().getContentAsString().indexOf("<td valign=\"top\" class=\"txtsmall\">Copyright &copy;2002-2014 Harris Computer Systems, Inc. All rights reserved.</td>"));
+            HtmlImageGenerator generator = new HtmlImageGenerator();
+            generator.setSize(new Dimension(1000, 1000));
+            generator.loadHtml(html);
+            File file = new File("grade.png");
+            generator.saveAsImage(file);
+
+            return file;
+        }catch (Exception ignore){
+            return null;
         }
     }
 
@@ -157,6 +197,7 @@ public class SchoolMax{
             if(classCode.get(i).startsWith("L"))
                 classCode.remove(i);
         }
+
 
         ArrayList<String> allGrade = new ArrayList<>();
         for(int i = 0; i < classCode.size(); i++){
@@ -225,6 +266,74 @@ public class SchoolMax{
     }
 
     public String checkAllFY() throws IOException {
+        ArrayList<String> every = (ArrayList<String>) this.checkAll().clone();
+        for(int i = 0; i < every.size(); i++){
+            int first = every.get(i).indexOf(" Grade Weighted") + 27;
+            int last = every.get(i).indexOf("%") + 1;
+            try {
+                every.set(i, every.get(i).substring(first, last));
+            }catch (StringIndexOutOfBoundsException ignore){}
+        }
+
+        //open gradebook
+        HtmlAnchor gradeBook = (HtmlAnchor) page.getAnchorByText("Gradebook");
+        page = gradeBook.click();
+
+        String all = page.getVisibleText();
+        String newAll = all.substring(all.indexOf("Instructor(s)") + 15, (all.lastIndexOf("(primary)") + 7)); //special +7 to include primar
+        String noGrade = newAll.replaceAll("\\[Grades]", "");
+        String noAssignment = noGrade.replaceAll("\\[Assignments]", "");
+        //String noPrimary = noAssignment.replaceAll("\\(primary", "");
+        //String noPar = noPrimary.replaceAll("\\)", "");
+        BufferedReader br = new BufferedReader(new StringReader(noAssignment));
+        String line;
+        ArrayList<String> allNames = new ArrayList<>();
+        while((line = br.readLine()) != null){
+            if((!(line.startsWith("LUNCH")) && line.contains("primar")) || line.contains("Released")){
+                if(!(line.trim().isEmpty())){
+                    if(!(line.contains("primary"))){   //to fix the primary problem
+                        line += "y)";
+                    }
+                    allNames.add(line);
+                }
+            }
+        }
+
+        int difference = 0;
+        if(allNames.size() != every.size()){
+            difference = Math.abs(allNames.size() - every.size());
+        }
+
+        while(difference != 0){
+            if(allNames.size() >= every.size()){
+                every.add("");
+            }else {
+                allNames.add("");
+            }
+            difference--;
+        }
+
+        for(int i = 0; i < allNames.size(); i++){
+            allNames.set(i, allNames.get(i) + ": " + every.get(i));
+        }
+        String all2 = "";
+        for(int i = 0; i < allNames.size(); i++){
+            all2 += allNames.get(i) + "\n\n";
+        }
+        return all2;
+    }
+
+    public String checkAllFYOther(String year) throws IOException {
+        try {
+            HtmlAnchor change = (HtmlAnchor) page.getAnchorByText("change");
+            page = change.click();
+            HtmlInput input = page.getFirstByXPath("//*[@id=\"field_year3\"]");
+            input.setValueAttribute(year);
+            HtmlSubmitInput button = (HtmlSubmitInput) page.getFirstByXPath("//*[@id=\"section_4\"]/tr[2]/td/input");
+            page = button.click();
+        }catch (Exception e){
+            return "Error";
+        }
         ArrayList<String> every = (ArrayList<String>) this.checkAll().clone();
         for(int i = 0; i < every.size(); i++){
             int first = every.get(i).indexOf(" Grade Weighted") + 27;
